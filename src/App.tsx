f7da5f7f442f796d647759dfd4713c3ac6da18b0
ainochi-kor <kthrkdals@gmail.com>
2024-05-04 14:36:29 +0900

    test: kebabCaseToTitleCase

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import { useState } from "react";
 import "./App.css";
+import { kebabCaseToTitleCase } from "./helpers";
 
 function App() {
   const [disabled, setDisabled] = useState(false);
-  const [buttonColor, setButtonColor] = useState("red");
-  const nextColor = buttonColor === "red" ? "blue" : "red";
+  const [buttonColor, setButtonColor] = useState("medium-violet-red");
+  const nextColorClass =
+    buttonColor === "medium-violet-red"
+      ? "medium-violet-blue"
+      : "medium-violet-red";
+  const nextColorTitleCalse = kebabCaseToTitleCase(nextColorClass);
   const className = disabled ? "gray" : buttonColor;
 
   return (
     <div>
       <button
         className={className}
-        onClick={() => setButtonColor(nextColor)}
+        onClick={() => setButtonColor(nextColorClass)}
         disabled={disabled}
       >
-        Change to {nextColor}
+        Change to {nextColorTitleCalse}
       </button>
       <br />
       <input
